14d07c7dc732768901f61469f6596084058bc024
RyanV-ponto <0001128421@senaimgaluno.com.br>
2026-07-02 20:10:10 -0300

    mudando rotras

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('index', 'Home::index');
 
 
-$routes->get('login', 'Home::login');
+$routes->get('/', 'Home::login');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,22 +1,10 @@
 <?php
-
 namespace App\Controllers;
 
+use codenova\Models;
+
 class Home extends BaseController
-{
-    public function index(): string
     {
-        
-        #return view('minha_primeira_pagina');
-         return view('welcome_message');
-
-    }
-<?php
-
-namespace App\Controllers;
-
-class Home extends BaseController
-{
     public function index(): string
     {
         
@@ -32,12 +20,8 @@ class Home extends BaseController
          return view('login');
 
     }
-   
+    
+    }
 
     
-}
-
-   
-
-    
-}
+?>
